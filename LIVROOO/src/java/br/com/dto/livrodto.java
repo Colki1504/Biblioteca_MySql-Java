05eefc1409a030200ca8727bd33e62a82992731d
Colki1504 <86538767+Colki1504@users.noreply.github.com>
2023-06-19 22:20:53 -0300

    Update livrodto.java

--- LIVROOO/src/java/br/com/dto/livrodto.java
+++ LIVROOO/src/java/br/com/dto/livrodto.java
@@ -1,6 +1,5 @@
 /*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ * Aqui eu usei variaveis publicas(public), porem você pode usar get/set, melhor dizendo é recomendado. 
  */
 package br.com.dto;
 
